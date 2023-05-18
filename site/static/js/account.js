c475d688b1e2d42bc83404d0df88e7844cfc3233
ZhangChengLin <469946668@qq.com>
2023-05-18 22:11:17 +0800

    Initial commit

--- site/static/js/account.js
+++ site/static/js/account.js
@@ -484,6 +484,7 @@ if (reset_password_email) {
 let reset_password_sms = document.querySelector('#reset_password_sms')
 if (reset_password_sms) {
     listenCaptcha(reset_password_sms)
+    listenIdd_code(reset_password_sms)
     listenSendBtn(reset_password_sms)
 
     reset_password_sms.addEventListener('submit', event => {
