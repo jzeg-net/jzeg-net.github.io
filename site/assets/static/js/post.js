b14ac1f65f5f7d04b0c1b50505649c2c782a3ca0
ZhangChengLin <469946668@qq.com>
2023-06-14 16:23:11 +0800

    Initial commit

--- site/assets/static/js/post.js
+++ site/assets/static/js/post.js
@@ -2,49 +2,53 @@ lazyLoad('.lazyImg')
 
 let tableOfContentsPin = document.querySelector('#tableOfContents-pin')
 if (tableOfContentsPin) {
-    let postNavbar = document.querySelector('#postNavbar')
+  let postNavbar = document.querySelector('#postNavbar')
 
-    tableOfContentsPin.addEventListener('click', () => {
-        tableOfContentsPin.childNodes.forEach(currentChild => {
-            currentChild.classList.toggle('d-none')
-        })
-        postNavbar.classList.toggle('sticky-top')
+  tableOfContentsPin.addEventListener('click', () => {
+    tableOfContentsPin.childNodes.forEach(currentChild => {
+      currentChild.classList.toggle('d-none')
     })
+    postNavbar.classList.toggle('sticky-top')
+  })
 }
 
 let sidebarCollapse = document.querySelector('#sidebar-collapse')
 if (sidebarCollapse) {
-    sidebarCollapse.addEventListener('click', () => {
-        sidebarCollapse.childNodes.forEach(currentChild => {
-            currentChild.classList.toggle('d-none')
-        })
+  sidebarCollapse.addEventListener('click', () => {
+    sidebarCollapse.childNodes.forEach(currentChild => {
+      currentChild.classList.toggle('d-none')
     })
+  })
 }
 
 // 长内容伸缩按钮
 let postContentMore = document.querySelector('#post-content-more')
 if (postContentMore) {
-    postContentMore.addEventListener('click', (event) => {
-        if ('DIV' === event.target.tagName) return
+  postContentMore.addEventListener('click', (event) => {
+    if ('DIV' === event.target.tagName) return
 
-        let postContent = document.querySelector('#post-content')
-        postContent.classList.toggle('article_shrink')
+    let postContent = document.querySelector('#post-content')
+    postContent.classList.toggle('article_shrink')
 
-        postContentMore.classList.toggle('shrink-post-content')
-        postContentMore.classList.toggle('sticky-bottom')
+    postContentMore.classList.toggle('shrink-post-content')
+    postContentMore.classList.toggle('sticky-bottom')
 
-        postContentMore.childNodes.forEach(currentChild => {
-            if ('BUTTON' === currentChild.tagName) {
-                currentChild.classList.toggle('d-none')
+    postContentMore.childNodes.forEach(currentChild => {
+      if ('BUTTON' === currentChild.tagName) {
+        currentChild.classList.toggle('d-none')
 
-                if (!currentChild.classList.contains('d-none')) {
-                    setTimeout(function () {
-                        // currentChildEl.focus({ preventScroll: false })
-                        currentChild.scrollIntoView(false)
-                    }, 400)
-                }
+        if (!currentChild.classList.contains('d-none')) {
+          setTimeout(function () {
+            // currentChildEl.focus({ preventScroll: false })
+            currentChild.scrollIntoView({
+              behavior: 'smooth',
+              block: 'end',
+              inline: 'center',
+            })
+          }, 400)
+        }
 
-            }
-        })
+      }
     })
+  })
 }
