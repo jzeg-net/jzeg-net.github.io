adbb7ff4440b4b75481b4041d051343a456f0b7a
ZhangChengLin <469946668@qq.com>
2023-06-21 00:59:17 +0800

    Initial commit

--- site/assets/static/js/concat/headerNotice.js
+++ site/assets/static/js/concat/headerNotice.js
@@ -1,6 +1,14 @@
 let headerNotice = document.querySelector('#headerNotice')
 
 if (headerNotice) {
+  headerNotice.addEventListener('close.bs.alert', (event) => {
+    let parent = event.target.parentNode
+    setTimeout(() => {
+      parent.parentNode.removeChild(parent)
+    }, 300)
+
+  })
+
   headerNotice.addEventListener('closed.bs.alert', (event) => {
     let close = {
       'close': Date.now().toString()
