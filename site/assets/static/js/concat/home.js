dbf5b3c9a6d993fc3a731301684ed491a894dd6c
ZhangChengLin <469946668@qq.com>
2023-06-22 23:47:29 +0800

    Initial commit

--- site/assets/static/js/concat/home.js
+++ site/assets/static/js/concat/home.js
@@ -16,12 +16,14 @@ let glideOpts = {
   startAt: 0,
   peek: 0,
   perView: 1,
-  animationTimingFunc: animationTimingFuncOpts['default'],
+  gap: 10, // A size of the gap added between slides
+  animationTimingFunc: animationTimingFuncOpts['ease'],
   animationDuration: 2000,
   throttle: 25, // Throttle costly events at most once per every wait milliseconds
   direction: htmlDir || 'ltr',
   rewindDuration: 800,
   autoplay: 6000, // Number | false
+  focusAt: 'center', // center | Number 0
   hoverpause: true,
   keyboard: true,
 }
@@ -29,3 +31,7 @@ let glideOpts = {
 let main_glide = new Glide('.main_glide', glideOpts)
 
 main_glide.mount()
+
+let grid_glide = new Glide('.grid_glide', glideOpts)
+
+grid_glide.mount()
