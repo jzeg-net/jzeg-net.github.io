b61677397d6872ba919f42da9238fa95ba2fd00e
ZhangChengLin <469946668@qq.com>
2023-08-30 13:33:25 +0800

    Initial commit

--- site/static/js/zhgh/sDataTables.js
+++ site/static/js/zhgh/sDataTables.js
@@ -22,7 +22,7 @@ let simpleDatatables_classes_bootstrap = {
   paginationListItemLink: 'page-link',
   search: 'datatable-search',
   selector: 'form-selector',
-  sorter: 'btn btn-sm btn-outline-secondary datatable-sorter',
+  sorter: 'datatable-sorter',
   table: 'table',
   top: 'datatable-top',
   wrapper: 'datatable-wrapper'
