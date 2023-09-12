49eb9e50742652ab5407d8df97567f2544819721
ZhangChengLin <469946668@qq.com>
2023-09-12 20:57:32 +0800

    Initial commit

--- site/assets/static/js/contact.js
+++ site/assets/static/js/contact.js
@@ -1,4 +1,30 @@
 let contact = document.querySelector('#contact')
 if (contact) {
 
+  contact.addEventListener('submit', event => {
+    event.preventDefault()
+    submitStatus(contact)
+    let formData = getFormData(contact)
+
+    let fetchData = {
+      type: formData['type'],
+      title: formData['title'],
+      content: formData['content'],
+      attachment: formData['attachment'],
+      captcha: formData['captcha'],
+      userAgent: navigator.userAgent,
+    }
+    let fetchOptions = fetchPostOptions(fetchData)
+
+    fetch(`https://api.zhgh.jzeg.net/contact/index.php`, fetchOptions)
+      .then(response => response.json())
+      .then(response => {
+        console.log(response)
+        clearFormSpinner(contact)
+      })
+      .catch(error => {
+        console.error('contact_error:', error)
+        clearFormSpinner(contact)
+      })
+  })
 }
