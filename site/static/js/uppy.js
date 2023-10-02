a3b5b210557c6e51ce58b07aa8de96bbc91aa637
ZhangChengLin <469946668@qq.com>
2023-10-02 15:09:14 +0800

    Initial commit

--- site/static/js/uppy.js
+++ site/static/js/uppy.js
@@ -3,25 +3,65 @@ let UppyLocale = {
   'en_US': Uppy.locales.en_US,
 }
 
+// let TUS_ENDPOINT = 'https://master.tus.io/files/'
+let TUS_ENDPOINT = 'https://tusd.tusdemo.net/files/'
+let XHR_ENDPOINT = 'https://api.xxxxxx.jzeg.net/shop/upload.php'
+
 let DashboardOpts = {
   inline: true,
+  autoProceed: true,
   target: '#uppy',
   theme: 'dark',
   disableLocalFiles: false,
   proudlyDisplayPoweredByUppy: false
 }
 
+let FormOpts = {
+  target: '#shop_add',
+  resultName: 'photoHash',
+  getMetaFromForm: false
+}
+
+let WebcamOpts = {
+  target: Uppy.Dashboard,
+  preferredImageMimeType: 'image/webp',
+  preferredVideoMimeType: 'video/webm'
+}
+
+let ScreenCaptureOpts = {
+  target: Uppy.Dashboard,
+  preferredVideoMimeType: 'video/webm'
+}
+
+let FileInputOpts = {
+  target: Uppy.Dashboard
+}
+
+let ImageEditorOpts = {
+  target: Uppy.Dashboard
+}
+
 let uppy = new Uppy.Uppy({
   locale: UppyLocale.zh_CN,
+  restrictions: {
+    maxFileSize: 111111,
+    minFileSize: 111111,
+    maxTotalFileSize: 111111,
+    maxNumberOfFiles: 111111,
+    minNumberOfFiles: 111111,
+    allowedFileTypes: ['image/*', '.webp', '.jpg', '.jpeg', '.png', '.gif'],
+    requiredMetaFields: [],
+  },
 })
   .use(Uppy.Dashboard, DashboardOpts)
-  .use(Uppy.Url, { target: Uppy.Dashboard, companionUrl: 'https://your-companion.com' })
-  .use(Uppy.ScreenCapture, { target: Uppy.Dashboard })
-  .use(Uppy.Webcam, { target: Uppy.Dashboard })
-  .use(Uppy.ImageEditor, { target: Uppy.Dashboard })
-  // .use(Uppy.Tus, { endpoint: 'https://master.tus.io/files/' })
-  // .use(Uppy.Tus, { endpoint: 'https://api.xxxxxx.jzeg.net/shop/upload.php' })
-  .use(Uppy.XHRUpload, { endpoint: 'https://api.xxxxxx.jzeg.net/shop/upload.php' })
+  .use(Uppy.FileInput, FileInputOpts)
+  .use(Uppy.Webcam, WebcamOpts)
+  .use(Uppy.ScreenCapture, ScreenCaptureOpts)
+  .use(Uppy.ImageEditor, ImageEditorOpts)
+  .use(Uppy.Form, FormOpts)
+  // .use(Uppy.Tus, { endpoint: TUS_ENDPOINT })
+  // .use(Uppy.Tus, { endpoint: XHR_ENDPOINT })
+  .use(Uppy.XHRUpload, { endpoint: XHR_ENDPOINT })
 
 uppy.on('complete', (result) => {
   console.log('上传完成！我们已经上传了这些文件：', result.successful)
