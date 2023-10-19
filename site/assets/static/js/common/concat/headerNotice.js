7e8f4cd9c664d51346ad0734e27bb6a76aba8220
ZhangChengLin <469946668@qq.com>
2023-10-19 18:35:20 +0800

    Initial commit

--- site/assets/static/js/common/concat/headerNotice.js
+++ site/assets/static/js/common/concat/headerNotice.js
@@ -1,18 +1,34 @@
 let headerNotice = document.querySelector('#headerNotice')
 
 if (headerNotice) {
+  const setLocalStorage = (key, value) => {
+    localStorage.setItem(key, value)
+  }
+
+  const getLocalStorage = (key) => {
+    return localStorage.getItem(key) ? localStorage.getItem(key) : false
+  }
+
+  let storageKey = 'headerNotice'
+  let storageValue = getLocalStorage(storageKey)
+
+  if (storageValue) {
+    let oneDay = 60 * 60 * 24 * 1e3
+    let intervals = Date.now() - storageValue
+
+    if (intervals <= oneDay) {
+      bootstrap.Alert.getOrCreateInstance(headerNotice).close()
+    }
+  }
+
   headerNotice.addEventListener('close.bs.alert', (event) => {
     let parent = event.target.parentNode
     setTimeout(() => {
       parent.parentNode.removeChild(parent)
-    }, 300)
-
+    }, 1e3)
   })
 
-  headerNotice.addEventListener('closed.bs.alert', (event) => {
-    let close = {
-      'close': Date.now().toString()
-    }
-    localStorage.setItem('headerNotice', encodeURI(JSON.stringify(close)))
+  headerNotice.addEventListener('closed.bs.alert', () => {
+    setLocalStorage(storageKey, Date.now())
   })
 }
