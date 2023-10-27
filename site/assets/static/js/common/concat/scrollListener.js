3810a5d8a81ee3b14ba64a46e2d0fce0f853f47e
ZhangChengLin <469946668@qq.com>
2023-10-27 13:40:31 +0800

    Initial commit

--- site/assets/static/js/common/concat/scrollListener.js
+++ site/assets/static/js/common/concat/scrollListener.js
@@ -1,37 +1,38 @@
 // 滚动监听
-let new_scroll_position = 0
-let last_scroll_position
-scrollListener()
+(() => {
+  'use strict'
 
-function scrollListener () {
+  let new_scroll_position = 0
+  let last_scroll_position
+
   document.addEventListener('scroll', () => {
     last_scroll_position = scrollY
     scrollMainNavbar()
     scrollFloatTools()
     new_scroll_position = last_scroll_position
   })
-}
 
-function scrollFloatTools () {
-  let floatTools = document.querySelector('#float-tools')
-  if (!floatTools) return
-  let windowOuterHeight = window.outerHeight
+  function scrollFloatTools () {
+    let floatTools = document.querySelector('#float-tools')
+    if (!floatTools) return
+    let windowOuterHeight = window.outerHeight
 
-  if (new_scroll_position < last_scroll_position || last_scroll_position < windowOuterHeight) {
-    floatTools.classList.add('hide')
-    getToolsCollapse().classList.remove('show')
-  } else if (new_scroll_position > last_scroll_position && last_scroll_position > windowOuterHeight) {
-    floatTools.classList.remove('hide')
+    if (new_scroll_position < last_scroll_position || last_scroll_position < windowOuterHeight) {
+      floatTools.classList.add('hide')
+      getToolsCollapse().classList.remove('show')
+    } else if (new_scroll_position > last_scroll_position && last_scroll_position > windowOuterHeight) {
+      floatTools.classList.remove('hide')
+    }
   }
-}
 
-function scrollMainNavbar () {
-  let navbar = document.querySelector('#mainNavbar')
-  if (!navbar) return
+  function scrollMainNavbar () {
+    let navbar = document.querySelector('#mainNavbar')
+    if (!navbar) return
 
-  if (new_scroll_position < last_scroll_position && last_scroll_position > 67) {
-    navbar.classList.add('tY-n150')
-  } else if (new_scroll_position > last_scroll_position) {
-    navbar.classList.remove('tY-n150')
+    if (new_scroll_position < last_scroll_position && last_scroll_position > 67) {
+      navbar.classList.add('tY-n150')
+    } else if (new_scroll_position > last_scroll_position) {
+      navbar.classList.remove('tY-n150')
+    }
   }
-}
+})()
