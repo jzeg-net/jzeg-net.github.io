ec14a670b72c1085133c3a512d5d150566eb270f
ZhangChengLin <469946668@qq.com>
2023-11-01 21:08:18 +0800

    Initial commit

--- site/assets/static/js/common/concat/disable-devtool.js
+++ site/assets/static/js/common/concat/disable-devtool.js
@@ -1,1 +1,10 @@
-DisableDevtool()
+if (DisableDevtool) {
+  let md5 = DisableDevtool.md5('xxx')
+
+  DisableDevtool({
+    disableMenu: false,
+    clearLog: false,
+    md5: md5,
+    tkName: 'false',
+  })
+}
