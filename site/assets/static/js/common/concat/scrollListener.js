21743d3aec82bba04647f7da5090c36a31e2d76f
ZhangChengLin <469946668@qq.com>
2023-11-14 15:58:12 +0800

    Initial commit

--- site/assets/static/js/common/concat/scrollListener.js
+++ site/assets/static/js/common/concat/scrollListener.js
@@ -2,25 +2,32 @@
 (() => {
   'use strict'
 
+  let windowOuterHeight = window.outerHeight
   let new_scroll_position = 0
   let last_scroll_position
+  let scroll_distance
 
   document.addEventListener('scroll', () => {
     last_scroll_position = scrollY
+    scroll_distance = last_scroll_position - new_scroll_position
+
     scrollMainNavbar()
     scrollFloatTools()
-    new_scroll_position = last_scroll_position
+
+    setTimeout(() => {
+      new_scroll_position = last_scroll_position
+    }, 350)
   })
 
   function scrollFloatTools () {
     let floatTools = document.querySelector('#float-tools')
     if (!floatTools) return
-    let windowOuterHeight = window.outerHeight
+    let isFirstPage = last_scroll_position < windowOuterHeight
 
-    if (new_scroll_position < last_scroll_position || last_scroll_position < windowOuterHeight) {
+    if (new_scroll_position < last_scroll_position || isFirstPage) {
       floatTools.classList.add('hide')
       getToolsCollapse().classList.remove('show')
-    } else if (new_scroll_position > last_scroll_position && last_scroll_position > windowOuterHeight) {
+    } else if (new_scroll_position > last_scroll_position && !isFirstPage) {
       floatTools.classList.remove('hide')
     }
   }
@@ -31,7 +38,9 @@
 
     if (new_scroll_position < last_scroll_position && last_scroll_position > 50) {
       navbar.classList.add('tY-n150')
-    } else if (new_scroll_position > last_scroll_position) {
+    } else if (new_scroll_position > last_scroll_position && Math.abs(scroll_distance) > 300) {
+      navbar.classList.remove('tY-n150')
+    } else if (last_scroll_position < windowOuterHeight) {
       navbar.classList.remove('tY-n150')
     }
   }
