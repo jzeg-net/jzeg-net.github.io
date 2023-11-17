3cea84020c4c457cc646675a324ce05a8fdcb101
ZhangChengLin <469946668@qq.com>
2023-11-17 21:35:48 +0800

    Initial commit

--- site/static/js/photoswipe.js
+++ site/static/js/photoswipe.js
@@ -1,12 +1,13 @@
 import PhotoSwipeLightbox from '/static/vendor/photoswipe/dist/photoswipe-lightbox.esm.min.js'
 
 const lightbox = new PhotoSwipeLightbox({
-  gallery:'.gallery',
+  gallery: '.gallery',
   children: '.img-thumbnail',
   bgOpacity: 0.8,
-  pswpModule:()=>import('/static/vendor/photoswipe/dist/photoswipe.esm.min.js'),
   preloadFirstSlide: false,
   loop: true,
-  spacing: 0.5 // 50% of viewport width
-});
-lightbox.init();
+  spacing: 0.5, // 50% of viewport width
+  pswpModule: () => import('/static/vendor/photoswipe/dist/photoswipe.esm.min.js')
+})
+
+lightbox.init()
