e312071000b56e3e27593c150ad01cf466249eb6
ZhangChengLin <469946668@qq.com>
2023-12-21 00:10:57 +0800

    Initial commit

--- site/assets/static/js/font-size.js
+++ site/assets/static/js/font-size.js
@@ -1,80 +1,101 @@
-/*!
- * Color mode toggler for Bootstrap's docs (https://getbootstrap.com/)
- * Copyright 2011-2023 The Bootstrap Authors
- * Licensed under the Creative Commons Attribution 3.0 Unported License.
- */
-
 (() => {
   'use strict'
 
   const getStoredFontSize = () => localStorage.getItem('font_size')
   const setStoredFontSize = font_size => localStorage.setItem('font_size', font_size)
   const body = document.body
+  let fontSizeSliderValues = ['3xs', '2xs', 'xs', 'sm', 'md', 'lg', 'xl', '2xl', '3xl', '4xl', '5xl', '6xl', '7xl', '8xl', '9xl']
+  let fontSizeSlider = document.querySelectorAll('.fontSizeSlider')
+
+  let fontSizeRangeInput = document.querySelectorAll('.fontSizeRangeInput')
 
   const getPreferredFontSize = () => getStoredFontSize() || 'md'
 
+  const getRealSize = (font_size) => {
+    return window.getComputedStyle(body).getPropertyValue('--jg--fs-' + font_size)
+  }
+
+  const setFontSizeInputValue = (fsValue) => {
+    let index = fontSizeSliderValues.indexOf(fsValue)
+
+    fontSizeSlider.forEach((sliderEL) => {
+      sliderEL.value = index
+    })
+  }
+
   const setFontSize = font_size => {
-    const size = window.getComputedStyle(body).getPropertyValue('--jg--fs-' + font_size)
+    const realSize = getRealSize(font_size)
     document.documentElement.setAttribute('data-bs-font_size', font_size)
-    body.style.setProperty('--bs-body-font-size', size)
+    body.style.setProperty('--bs-body-font-size', realSize)
+    setStoredFontSize(font_size)
+  }
+
+  const listenFontSizeRangeInputChange = () => {
+    fontSizeRangeInput.forEach((rangeInputEL) => {
+      rangeInputEL.addEventListener('change', (event) => {
+        console.log(event)
+        console.log(rangeInputEL.value)
+      })
+    })
   }
 
   const showCurrentValue = () => {
     const currentFontSizeValue = window.getComputedStyle(body).getPropertyValue('font-size')
-    const msg = '字体尺寸调整为：' + currentFontSizeValue
+    const currentFontSize = document.querySelectorAll('.currentFontSize')
 
-    bModal('', createSmallCenterText(msg, 'success'), '', 'sm', true)
+    currentFontSize.forEach((fsEL) => {
+      fsEL.innerHTML = currentFontSizeValue
+    })
+  }
+
+  const sliderController = () => {
+    let fontSizeSliderController = document.querySelectorAll('.fontSizeSliderController')
+    let fontSizeIndex = fontSizeSliderValues.indexOf(getPreferredFontSize())
+
+    fontSizeRangeInput.forEach(EL => {
+      EL.value = fontSizeIndex
+    })
+
+    fontSizeSliderController.forEach((contrEL) => {
+      contrEL.addEventListener('click', () => {
+        if (contrEL.tagName !== 'A') return
+
+        let fontSizeIndex = fontSizeSliderValues.indexOf(getPreferredFontSize())
+
+        if (contrEL.classList.contains('controllerUp')) {
+          fontSizeIndex++
+        } else if (contrEL.classList.contains('controllerDown')) {
+          fontSizeIndex--
+        }
+
+        if (fontSizeIndex >= fontSizeSliderValues.length) return
+
+        fontSizeRangeInput.forEach(EL => {
+          EL.value = fontSizeIndex
+        })
+
+        setStoredFontSize(fontSizeSliderValues[fontSizeIndex])
+        setFontSize(getPreferredFontSize())
+        showCurrentValue()
+      })
+    })
   }
 
   setFontSize(getPreferredFontSize())
 
-  const showActiveFontSize = (font_size, focus = false) => {
-    const font_sizeSwitcher = document.querySelector('[data-bs-font_size="switcher"]')
-
-    if (!font_sizeSwitcher) {
-      return
-    }
-
-    const font_sizeSwitcherText = document.querySelector('#font_sizeSwitcher-text')
-    const activeFontSizeIcon = document.querySelector('.font_size-icon-active use')
-    const btnToActive = document.querySelector(`[data-bs-font_size-value="${font_size}"]`)
-    const svgOfActiveBtn = btnToActive.querySelector('svg use').getAttribute('href')
-
-    document.querySelectorAll('[data-bs-font_size-value]').forEach(element => {
-      element.classList.remove('active')
-      element.setAttribute('aria-pressed', 'false')
-    })
-
-    btnToActive.classList.add('active')
-    btnToActive.setAttribute('aria-pressed', 'true')
-    // activeFontSizeIcon.setAttribute('href', svgOfActiveBtn)
-    // const font_sizeSwitcherLabel = `${font_sizeSwitcherText.textContent} (${btnToActive.title})`
-    // font_sizeSwitcher.setAttribute('aria-label', font_sizeSwitcherLabel)
-
-    if (focus) {
-      font_sizeSwitcher.focus()
-    }
-  }
-
-  window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', () => {
-    const storedFontSize = getStoredFontSize()
-    if (storedFontSize !== 'light' || storedFontSize !== 'dark') {
-      setFontSize(getPreferredFontSize())
-    }
-  })
-
   window.addEventListener('DOMContentLoaded', () => {
-    showActiveFontSize(getPreferredFontSize())
-
-    document.querySelectorAll('[data-bs-font_size-value]')
-      .forEach(toggle => {
-        toggle.addEventListener('click', () => {
-          const font_size = toggle.getAttribute('data-bs-font_size-value')
-          setStoredFontSize(font_size)
-          setFontSize(font_size)
-          showCurrentValue()
-          showActiveFontSize(font_size, true)
-        })
-      })
+    sliderController()
+    listenFontSizeRangeInputChange()
+    setFontSizeInputValue(getPreferredFontSize)
+    showCurrentValue()
   })
+
+  window.addEventListener('storage', (e) => {
+    console.log(e)
+    let fontSize = getPreferredFontSize()
+    setFontSize(fontSize)
+    setFontSizeInputValue(fontSize)
+    showCurrentValue()
+  })
+
 })()
