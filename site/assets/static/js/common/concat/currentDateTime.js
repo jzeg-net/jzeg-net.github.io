38bc4aa0e331fbe21b3ea3aa1084c80921568a2a
ZhangChengLin <469946668@qq.com>
2023-12-31 22:48:06 +0800

    Initial commit

---
+++ site/assets/static/js/common/concat/currentDateTime.js
@@ -0,0 +1,22 @@
+(() => {
+  'use strict'
+
+  let now
+  const pageLanguageCode = document.querySelector('html').getAttribute('lang').toLowerCase()
+
+  window.addEventListener('DOMContentLoaded', () => {
+    const currentDateTime = document.querySelectorAll('.currentDateTime')
+
+    currentDateTime.forEach((dtEL) => {
+      setInterval(() => {
+        now = dayjs()
+        dtEL.innerHTML = now.locale(pageLanguageCode).format('YYYY-MM-DD HH:mm:ss dddd')
+      }, 1000)
+    })
+  })
+
+  window.addEventListener('beforeunload', function (event) {
+    localStorage.setItem('last_visited', dayjs(now).unix())
+  })
+
+})()
