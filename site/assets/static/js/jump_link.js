e39026544f0a58895a0eacc8bf014b2dfe51a9b4
ZhangChengLin <469946668@qq.com>
2024-03-07 23:30:42 +0800

    Initial commit

--- site/assets/static/js/jump_link.js
+++ site/assets/static/js/jump_link.js
@@ -2,9 +2,26 @@
   'use strict'
 
   let linkInput = document.querySelector('#link_input')
+  let linkCopy = document.querySelector('#link_copy')
   let toBack = document.querySelector('#to_back')
   let toExternal = document.querySelector('#to_external')
   let dismissible = document.querySelector('#jump_link_dismissible')
+
+  linkCopy.addEventListener('click', (event) => {
+    let useHref = linkCopy.querySelector('use').href
+
+    const toggleuseHref = () => {
+      if (useHref.baseVal === '#bi-copy') {
+        useHref.baseVal = '#bi-check-square'
+      } else {
+        useHref.baseVal = '#bi-copy'
+      }
+    }
+
+    toggleuseHref()
+    setTimeout(toggleuseHref, 2500)
+
+  })
 
   toBack.addEventListener('click', () => {
     open(document.referrer, '_parent') // history.go(-1)
