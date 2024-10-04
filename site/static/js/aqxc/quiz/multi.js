55e3630c6bea50c0233ff28046fad49a579098eb
ZhangChengLin <469946668@qq.com>
2024-10-05 04:43:08 +0800

    Initial commit

--- site/static/js/aqxc/quiz/multi.js
+++ site/static/js/aqxc/quiz/multi.js
@@ -3,6 +3,73 @@ let url = aqxcApiUrl + 'quiz/multi'
 
 let loopCount = document.querySelector('#loopCount')
 
+// 计时器ID
+let submitTimerIntervalID
+
+// 设置计时器开启
+const setSubmitTimerInterval = () => {
+  let submit_timer = document.querySelector('#submit_timer')
+  submit_timer.textContent = '0'
+
+  submitTimerIntervalID = setInterval(() => {
+    submit_timer.textContent++
+  }, 1000)
+}
+
+// 重置计时器ID，并且清除计时器
+const clearSubmitTimerInterval = (intervalID) => {
+  let submit_timer = document.querySelector('#submit_timer')
+  submit_timer.textContent = '0'
+
+  clearInterval(intervalID)
+}
+
+// 提交按钮的状态效果
+const createNewSpinner = () => {
+  let spinner = document.createElement('span')
+  let spinnerVisuallyHidden = document.createElement('span')
+
+  spinner.className = 'me-2 spinner-border spinner-border-sm'
+  spinner.setAttribute('role', 'status')
+
+  spinnerVisuallyHidden.className = 'visually-hidden'
+  spinnerVisuallyHidden.innerText = '加载中'
+
+  spinner.appendChild(spinnerVisuallyHidden)
+
+  return spinner
+}
+
+const setInsertNewElement = (calledElement, newElement, where = 'afterbegin') => {
+  calledElement.insertAdjacentElement(where, newElement)
+}
+
+// 设置按钮的禁用状态
+const setBtnDisabledStatus = btnElement => {
+  btnElement.classList.toggle('disabled')
+  btnElement.toggleAttribute('disabled')
+}
+
+const setSubmitStatus = formEl => {
+  let submit = formEl.querySelector('[type=submit]')
+
+  setInsertNewElement(submit, createNewSpinner())
+  setBtnDisabledStatus(submit)
+}
+
+// 清除提交按钮中的加载图标
+const clearSubmitSpinner = submit => {
+  submit.querySelector('.spinner-border').remove()
+}
+
+// 清除提交按钮的状态效果
+const clearSubmitStatus = formEl => {
+  let submit = formEl.querySelector('[type=submit]')
+
+  setBtnDisabledStatus(submit)
+  clearSubmitSpinner(submit)
+}
+
 // 显示结果
 const displayQuizResults = data => {
   const { power, tester_score: score, tester_time: time, right_count: right, wrong_count: wrong } = data
@@ -20,7 +87,8 @@ const displayQuizResults = data => {
 // 处理表单提交
 const handleFormSubmit = event => {
   event.preventDefault()
-  submitStatus(multi_form)
+  setSubmitStatus(multi_form)
+  setSubmitTimerInterval()
   let formData = new FormData(multi_form)
   let data = Object.fromEntries(formData.entries())
   data.token = getStorageAqxcToken()
@@ -43,9 +111,11 @@ const handleFormSubmit = event => {
       }
 
       displayQuizResults(res.data)
+      clearSubmitStatus(multi_form)
+      clearSubmitTimerInterval(submitTimerIntervalID)
 
       // 检查 power 值和 loopCount 的值来决定是否再次提交
-      if (res.data.power >= 16 && loopCount.value > 1) {
+      if (res.data.power >= 2 && loopCount.value > 1) {
         // 递归调用
         handleFormSubmit(event)
         // 减少循环次数
@@ -53,7 +123,6 @@ const handleFormSubmit = event => {
       }
     })
     .finally(() => {
-      clearFormSpinner(multi_form)
     })
 }
 
