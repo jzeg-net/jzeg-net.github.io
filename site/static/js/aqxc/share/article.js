571a2301d56e3194e693ecf26e1d72c5901baf7a
ZhangChengLin <469946668@qq.com>
2025-05-14 00:08:08 +0800

    添加 分享资讯

---
+++ site/static/js/aqxc/share/article.js
@@ -0,0 +1,32 @@
+let shareArticle = document.querySelector('#shareArticle')
+let url = aqxcApiUrl + 'share/article'
+
+const request = () => {
+  let data = {
+    token: getStorageAqxcToken(),
+    account: getStorageAqxcAccount(),
+  }
+
+  fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+    },
+    body: JSON.stringify(data)
+  })
+    .then(res => res.json())
+    .then(res => {
+      // 如果同时存在消息和错误信息，则显示模态框并返回
+      if (res.hasOwnProperty('message') && (res.hasOwnProperty('code') || res.hasOwnProperty('errors'))) {
+        bModal('', createSmallCenterText(res.message, 'danger'), '', 'sm', true)
+        return
+      }
+
+      let successfullyRead = res.data['successfullyRead']
+      let msg = '有效阅读了' + successfullyRead + '篇文章'
+      bModal('', createSmallCenterText(msg, 'success'), '', 'sm', true)
+    })
+}
+
+shareArticle.addEventListener('click', request)
