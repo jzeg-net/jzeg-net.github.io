93f6160a7f1184028deeb66d43aa1d44c4d1831c
ZhangChengLin <469946668@qq.com>
2025-05-23 00:29:39 +0800

    新增 知识题库

---
+++ site/static/js/aqxc/questions/category.js
@@ -0,0 +1,94 @@
+let storageAqxcQuestionsCategoryKey = 'aqxcQuestionsCategory'
+
+const getStorageAqxcQuestionsCategory = () => getLocalStorage(storageAqxcQuestionsCategoryKey)
+
+const setStorageAqxcQuestionsCategory = (value) => setLocalStorage(storageAqxcQuestionsCategoryKey, value)
+
+const removeStorageAqxcQuestionsCategory = () => removeLocalStorage(storageAqxcQuestionsCategoryKey)
+
+let categorySelect = document.querySelector('#categorySelect')
+
+const createOption = data => {
+  let opt = document.createElement('option')
+  opt.value = data.value
+  opt.textContent = data.text
+
+  return opt
+}
+
+const populateSelectWithOptions = data => {
+  let fragment = document.createDocumentFragment()
+
+  Object.keys(data).forEach(key => {
+    let category_name = data[key]['exam_list'][0]['exam_title']
+    let category_id = data[key]['exam_list'][0]['exam_id']
+    let question_count = data[key]['exam_list'][0]['question_count']
+
+    let optionData = {
+      value: category_id,
+      text: category_name + `『 ${question_count} 』`,
+    }
+    let option = createOption(optionData)
+    fragment.appendChild(option)
+  })
+
+  return fragment
+}
+
+const getQuestionsCategory = () => {
+  let cachedCategory = JSON.parse(getStorageAqxcQuestionsCategory())
+
+  // 判断缓存是否可用
+  if (cachedCategory && (Date.now() < cachedCategory.expires)) {
+    // 使用缓存
+    categorySelect.append(populateSelectWithOptions(cachedCategory.data))
+    return
+  }
+
+  // 从API获取数据
+  let baseUrl = aqxcApiUrl + 'tester/category'
+  const queryParams = {
+    token: getStorageAqxcToken(),
+    account: getStorageAqxcAccount(),
+  }
+  const url = addQueryParams(baseUrl, queryParams)
+
+  fetch(url, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+    },
+  })
+    .then(res => res.json())
+    .then(res => {
+      // 如果有 code 和 message，则显示错误信息，并且后面不再执行后续代码
+      if (res.hasOwnProperty('code') && res.hasOwnProperty('message')) {
+        bModal('', createSmallCenterText(res.message, 'danger'), '', 'sm', true)
+
+        return
+      }
+      if (!res.hasOwnProperty('data')) {
+        bModal('', createSmallCenterText('没有从服务器获取到数据', 'danger'), '', 'sm', true)
+
+        return
+      }
+
+      let categoryData = res.data
+
+      let newCachedCategory = {
+        expires: Date.now() + (3600 * 1e3), // 缓存1小时
+        data: categoryData,
+      }
+      setStorageAqxcQuestionsCategory(JSON.stringify(newCachedCategory))
+
+      categorySelect.append(populateSelectWithOptions(categoryData))
+    })
+    .catch(err => {
+      console.log(err)
+    })
+}
+
+categorySelect.addEventListener('click', () => {
+  getQuestionsCategory()
+}, { once: true })
---
+++ site/static/js/aqxc/questions/index.js
@@ -0,0 +1,108 @@
+const questions_form = document.querySelector('#questions_form')
+
+const buildQuestionBankLayout = (data) => {
+  const fragment = document.createDocumentFragment()
+
+  data.forEach((item, index) => {
+    const { question_type, question_title, answer_list } = item
+    const letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    const questionIndex = index + 1
+
+    const p = document.createElement('p')
+    p.className = 'question-title mb-0'
+    p.innerHTML = questionIndex + '.  ' + question_title
+
+    const div = document.createElement('div')
+    div.className = 'question-options row row-cols-auto ms-3 mb-3'
+
+    answer_list.forEach((item, index) => {
+      const is_right = item['is_right'] === '1'
+      const a = document.createElement('a')
+      is_right
+        ? a.className = 'answer-option fw-bolder link-success link-offset-3'
+        : a.className = 'answer-option text-reset text-decoration-none'
+      a.innerHTML = letters[index] + '. ' + item['answer_content']
+
+      div.appendChild(a)
+    })
+
+    fragment.appendChild(p)
+    fragment.appendChild(div)
+  })
+
+  return fragment
+}
+
+const openPreview = (data) => {
+  const newWindow = window.open('', '_blank')
+
+  newWindow.document.write(`
+        <!DOCTYPE html>
+        <html lang="zh">
+        <head>
+          <title>题库</title>
+          <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet">
+          <style>
+          /* 通用打印样式 */
+          @media print {
+            .no-print { display: none !important; }
+            .question-card { break-inside: avoid; }
+            /* 定义页脚 */
+            @page {
+              @bottom-right {
+                content: "杰格网【jzeg.net】 倾情奉献";
+                font-size: 12px;
+                color: #888888;
+              }
+            }
+          }
+          </style>
+        </head>
+        <body class="container-fluid px-4">
+          <div class="fixed-bottom d-grid">
+            <button class="no-print btn btn-lg btn-success" type="button" onclick="window.print()">打印</button>
+          </div>
+          <div class="pb-5" id="content"></div>
+        </body>
+        </html>
+      `)
+
+  const contentNode = newWindow.document.getElementById('content')
+  const questionsDOM = buildQuestionBankLayout(data)
+  contentNode.appendChild(questionsDOM)
+  newWindow.document.close()
+}
+
+const getQuestions = (event) => {
+  event.preventDefault()
+  setSubmitStatus(questions_form)
+
+  let formData = new FormData(questions_form)
+  let data = Object.fromEntries(formData.entries())
+  data.account = getStorageAqxcAccount()
+  data.token = getStorageAqxcToken()
+
+  let url = aqxcApiUrl + 'questions/questions'
+  fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    },
+    body: JSON.stringify(data)
+  }).then(r => {
+    if (!r.ok) {
+      r.json().then(data => {
+        bModal('', createSmallCenterText(data.message, 'danger'), '', 'sm', true)
+      })
+      return Promise.reject(new Error(data.message))
+    }
+    return r.json()
+  }).then(r => {
+    openPreview(r)
+  }).finally(() => {
+    clearSubmitStatus(questions_form)
+  })
+}
+
+questions_form?.addEventListener('submit', getQuestions)
