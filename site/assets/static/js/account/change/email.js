1b07bc68255d6326ab52449172f4323f5c8f9262
ZhangChengLin <469946668@qq.com>
2025-06-18 11:11:34 +0800

    -

--- site/assets/static/js/account/change/email.js
+++ site/assets/static/js/account/change/email.js
@@ -1,36 +1,35 @@
-let change_email = document.querySelector('#change_email')
+const change_email = document.querySelector('#change_email')
 if (change_email) {
-    listenCaptcha(change_email)
-    listenSendBtn(change_email)
+  listenCaptcha(change_email)
+  listenSendBtn(change_email)
 
-    change_email.addEventListener('submit', event => {
-        event.preventDefault()
-        submitBtnStatus(change_email)
-        let formData = getFormData(change_email)
+  change_email.addEventListener('submit', event => {
+    event.preventDefault()
+    submitBtnStatus(change_email)
+    const formData = getFormData(change_email)
 
-        let fetchData = {
-            type: formData['type'],
-            account: formData['account'],
-            password: formData['password'],
-            account_captcha: formData['account_captcha'],
-            captcha: formData['captcha'],
-            token: getToken(),
+    const fetchData = {
+      type: formData['type'],
+      account: formData['account'],
+      password: formData['password'],
+      account_captcha: formData['account_captcha'],
+      captcha: formData['captcha'],
+      token: getToken(),
+    }
+    const fetchOptions = getFetchOptions(fetchData)
+
+    fetch(accountApiUrl + '/change.php', fetchOptions)
+      .then(response => response.json())
+      .then(response => {
+        if (response['code']) {
+          responseCode(response, change_email)
+        } else {
+          const x = bModal('', createSmallCenterText(response['msg'], 'success'), '', 'sm', true)
+          document.querySelector('#' + x).addEventListener('hidden.bs.modal', () => {
+            location.replace('/')
+          })
         }
-        let fetchOptions = getFetchOptions(fetchData)
-
-        fetch(accountApiUrl + '/change.php', fetchOptions)
-            .then(response => response.json())
-            .then(response => {
-                if (response['code']) {
-                    responseCode(response, change_email)
-                } else {
-                    let x = bModal('', createSmallCenterText(response['msg'], 'success'), '', 'sm', true)
-                    document.querySelector('#' + x).addEventListener('hidden.bs.modal', () => {
-                        location.replace('/')
-                    })
-                }
-            })
-            .catch(error => console.error('change_error:', error))
-
-    })
+      })
+      .catch(error => console.error('change_error:', error))
+  })
 }
