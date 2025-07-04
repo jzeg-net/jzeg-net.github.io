88085fac16b91f0fc0cae7a34a97cc6934a506e6
ZhangChengLin <469946668@qq.com>
2025-07-04 21:44:59 +0800

    -

--- site/static/js/zhgh/index.js
+++ site/static/js/zhgh/index.js
@@ -1,8 +1,24 @@
 const zhgh_login = document.querySelector('#zhgh_login')
 const member_information = document.querySelector('#member_information')
 
+member_information.innerHTML = `
+  <div class="card">
+    <div class="card-body text-center">登录后可查看更多信息</div>
+  </div>
+  `
+
+const formattedTime = (time) => {
+  return dayjs.unix(time).format('YYYY-MM-DD HH:mm:ss')
+}
+
 const getMemberInformation = () => {
   const url = `${zhghApiUrl}/member/information`
+
+  member_information.innerHTML = `
+  <div class="card">
+    <div class="card-body text-center">信息加载中，请稍后。</div>
+  </div>
+  `
 
   const fetchData = {
     'account': getStorageZhghAccount(),
@@ -54,37 +70,44 @@ const getMemberInformation = () => {
               <div>姓名：${true_name}</div>
               <div>账号：${id}</div>
               <div>单位：${group}</div>
-              <div>等级编号：${level}</div>
-              <div>等级：${level_name}</div>
-              <div>等级积分：${exp_points_2}</div>
-              <div>学习时长：${study_time}</div>
-              <div>积分：${points}</div>
-              <div>经验积分：${exp_points}</div>
-              <div>排行：${ranking}</div>
-              <div>生日：${birthday}</div>
-              <div>加入时间：${add_time}</div>
+              <div>学历：${xueli}</div>
+              <div>职位：${zhiwei}</div>
+              <div>政治面貌：${zzmm}</div>
               <div>民族：${min_zu}</div>
               <div>籍贯：${native}</div>
               <div>手机：${mobile}</div>
               <div>电话：${phone}</div>
-              <div>学历：${xueli}</div>
-              <div>职位：${zhiwei}</div>
-              <div>政治面貌：${zzmm}</div>
+              <hr>
+              <div>等级编号：${level}</div>
+              <div>等级：${level_name} （${exp_points_2}）</div>
+              <div>学习时长：${study_time}</div>
+              <div>积分：${points}</div>
+              <div>经验积分：${exp_points}</div>
+              <div>排行：${ranking}</div>
+              <div>生日：${formattedTime(birthday)}</div>
+              <div>加入时间：${formattedTime(add_time)}</div>
+              <hr>
               <div>登录次数：${login_num}</div>
               <div>登录IP：${login_ip}</div>
-              <div>登录时间：${login_time}</div>
-              <div>上次登录IP：${old_login_ip}</div>
-              <div>上次登录时间：${old_login_time}</div>
+              <div>登录时间：${formattedTime(login_time)}</div>
+              <div>上次IP：${old_login_ip}</div>
+              <div>上次时间：${formattedTime(old_login_time)}</div>
             </div>
           </div>
         `
     })
     .finally(() => {
     })
+    .catch(() => {
+      member_information.innerHTML = `
+      <div class="card">
+        <div class="card-body text-center">信息加载失败。</div>
+      </div>`
+    })
 
 }
 
-if (member_information) getMemberInformation()
+if (getStorageZhghAccount() && getStorageZhghPassword()) getMemberInformation()
 
 if (zhgh_login) {
   listenerPasswordInputType(zhgh_login)
@@ -116,6 +139,7 @@ if (zhgh_login) {
     if (password.value) setStorageZhghPassword(password.value)
 
     bModal('', createSmallCenterText('已更新账号信息', 'success'), '', 'sm', true)
+    getMemberInformation()
   })
 
   zhgh_login.addEventListener('reset', event => {
