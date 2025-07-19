e14c5eaa6a24216b8097809e2bb03e8e0fe40c83
ZhangChengLin <469946668@qq.com>
2025-07-19 22:56:54 +0800

    -

--- site/static/js/aqxc/favourite/index.js
+++ site/static/js/aqxc/favourite/index.js
@@ -1,15 +1,23 @@
 const favourite_form = document.querySelector('#favourite_form')
 
-favourite_form?.addEventListener('submit', (e) => {
-  e.preventDefault()
-  submitStatus(favourite_form)
+const newsHandler = (response) => {}
 
+const profileHandler = (response) => {}
 
-  const formData = new FormData(favourite_form)
-  const data = Object.fromEntries(formData.entries())
-  data.account = getStorageAqxcAccount()
-  data.token = getStorageAqxcToken()
+const videoHandler = (response) => {
+  const count = response['totalCount']
+  if (!count) {
+    bModal('', createSmallCenterText('没有收藏视频，无需取消', 'danger'), '', 'sm', true)
+    return
+  }
 
+  const list = response['list']
+  const msg = `成功取消收藏 ${count} 个视频`
+  bModal('', createSmallCenterText(msg, 'success'), '', 'sm', true)
+}
+
+const request = (data) => {
+  const type = data.type
   const url = aqxcApiExtendUrl + 'favourite/favourite'
 
   fetch(url, fetchPostOptions(data))
@@ -20,14 +28,40 @@ favourite_form?.addEventListener('submit', (e) => {
         })
         return Promise.reject(new Error(res.statusText))
       }
-
       return res.json()
     })
     .then(res => {
       console.log(res)
+
+      switch (type) {
+        case 'video':
+          videoHandler(res)
+          break
+        case 'news':
+          newsHandler(res)
+          break
+        case 'profile':
+          profileHandler(res)
+          break
+        default :
+          bModal('', createSmallCenterText('操作失败', 'danger'), '', 'sm', true)
+      }
+
     })
     .finally(() => {
       clearSubmitStatus(favourite_form)
     })
     .catch(error => console.error('favourite_error:', error))
+}
+
+favourite_form?.addEventListener('submit', (e) => {
+  e.preventDefault()
+  submitStatus(favourite_form)
+
+  const formData = new FormData(favourite_form)
+  const data = Object.fromEntries(formData.entries())
+  data.account = getStorageAqxcAccount()
+  data.token = getStorageAqxcToken()
+
+  request(data)
 })
