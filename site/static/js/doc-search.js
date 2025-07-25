750b7c32b4fd2ef2f13a55f949f40b9dae77ba57
ZhangChengLin <469946668@qq.com>
2025-07-25 12:50:15 +0800

    -

--- site/static/js/doc-search.js
+++ site/static/js/doc-search.js
@@ -1,6 +1,6 @@
 docsearch({
     container: '#docSearch',
-    appId: '',
-    indexName: '',
-    apiKey: '',
+    appId: '24b09689d5b4223813d9b8e48563c8f6',
+    indexName: 'docsearch',
+    apiKey: 'askAIDemo',
 })
