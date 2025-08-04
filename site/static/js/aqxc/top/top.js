8b44464f2107ad38de1ecd9a00c238cace4f35e0
ZhangChengLin <469946668@qq.com>
2025-08-04 17:02:38 +0800

    -

--- site/static/js/aqxc/top/top.js
+++ site/static/js/aqxc/top/top.js
@@ -2,7 +2,7 @@ let top_form = document.querySelector('#top_form')
 if (top_form) {
   let top_month = document.querySelector('#top_month')
   // 将当前月份设置为默认值
-  top_month.value = new Date().toISOString().slice(0, 7)
+  top_month.value = dayjs().format('YYYY-MM')
 
   top_form.addEventListener('submit', (e) => {
     e.preventDefault()
