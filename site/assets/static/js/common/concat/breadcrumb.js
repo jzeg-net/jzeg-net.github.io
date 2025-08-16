11c1a553c08df0760c406a1a45f5453331d9bc70
ZhangChengLin <469946668@qq.com>
2025-08-16 23:57:03 +0800

    -

--- site/assets/static/js/common/concat/breadcrumb.js
+++ site/assets/static/js/common/concat/breadcrumb.js
@@ -4,6 +4,5 @@ const breadcrumb_itemList = breadcrumb_container.querySelectorAll('li.breadcrumb
 breadcrumb_itemList.forEach((item, index) => {
   breadcrumb_titles[index] = item.querySelector('a')?.innerText
 })
-console.log(breadcrumb_titles)
-// 当前标题是数组中的最后一个
 const current_title = breadcrumb_titles[breadcrumb_titles.length - 1]
+const parent_title = breadcrumb_titles[breadcrumb_titles.length - 2]
--- site/assets/static/js/common/concat/snapdom.js
+++ site/assets/static/js/common/concat/snapdom.js
@@ -1,2 +1,8 @@
-const snapdomOpt = {
-}
+const snapdomOpt = {}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const snapdomDownloads = document.querySelectorAll('.snapdom_downloads')
+  snapdomDownloads.forEach((dom) => {
+    dom.addEventListener('click', snapdomDown)
+  })
+})
