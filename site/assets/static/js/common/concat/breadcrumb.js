d49e66dfba6400f7665a5e3b7bc4080efcb490bf
ZhangChengLin <469946668@qq.com>
2025-08-18 11:02:09 +0800

    -

--- site/assets/static/js/common/concat/breadcrumb.js
+++ site/assets/static/js/common/concat/breadcrumb.js
@@ -1,7 +1,7 @@
 const breadcrumb_container = document.querySelector('ol.breadcrumb')
 const breadcrumb_titles = []
-const breadcrumb_itemList = breadcrumb_container.querySelectorAll('li.breadcrumb-item')
-breadcrumb_itemList.forEach((item, index) => {
+const breadcrumb_itemList = breadcrumb_container?.querySelectorAll('li.breadcrumb-item')
+breadcrumb_itemList?.forEach((item, index) => {
   breadcrumb_titles[index] = item.querySelector('a')?.innerText
 })
 const current_title = breadcrumb_titles[breadcrumb_titles.length - 1]
